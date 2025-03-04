f43221b57423a49f7fcef34bece305f1dcf451b0
fst135207 <felix.stalder.bsu@gmail.com>
2025-03-04 14:24:27 +0100

    fixed more props errors

--- portfolio/components/Education.tsx
+++ portfolio/components/Education.tsx
@@ -39,7 +39,7 @@ export default function Experience() {
             <div className="relative">
               {/* Timeline line */}
               <div className="absolute left-1.5 top-5 w-0.5 bg-gray-700" style={{ height: 'calc(100% - 16rem)'}}/>
-              {educations.map((exp, index) => (
+              {educations.map((exp, id) => (
                 <div key={exp.id} className="mb-8 relative">
                   {/* Timeline dot */}
                   <div className="absolute left-0.5 top-3 w-3 h-3 bg-gray-700 rounded-full" />
--- portfolio/components/Project.tsx
+++ portfolio/components/Project.tsx
@@ -41,8 +41,8 @@ export default function ProjectCard({
           <div className="text-3xl text-primary">{icon}</div>
           <div>
             <CardTitle className="text-xl text-blue-500">{title}</CardTitle>
-            {technologies.map((tech, index) => (
-              <Badge key={index} variant="outline" className="mt-1 text-indigo-400 mr-1">
+            {technologies.map((tech, id) => (
+              <Badge key={id} variant="outline" className="mt-1 text-indigo-400 mr-1">
                 <span className="technology">{tech}</span>
               </Badge>
             ))}
